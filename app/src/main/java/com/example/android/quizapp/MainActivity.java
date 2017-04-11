3b6fd34e63bebd110a1e404103a36053abdb2d75
Rani <ranielmaalouf@gmail.com>
2017-04-11 20:13:38 +0200

    Add third question and darken background color of each question.

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.quizapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.RadioButton;
 import android.widget.Toast;
 
@@ -20,12 +21,23 @@ public class MainActivity extends AppCompatActivity {
 
         // Boolean for the correct answer of the first question
         boolean answer1 = ((RadioButton) findViewById(R.id.q1)).isChecked();
+
+        // Boolean for the correct answer of the second question
         boolean answer2 = ((RadioButton) findViewById(R.id.q2)).isChecked();
 
-        // Sum of correct answers
-        score = (answer1?1:0) + (answer2?1:0);
+        // Booleans for the checkboxes of third question
+        boolean checkBox1 = ((CheckBox) findViewById(R.id.q3_1)).isChecked();
+        boolean checkBox2 = ((CheckBox) findViewById(R.id.q3_2)).isChecked();
+        boolean checkBox3 = ((CheckBox) findViewById(R.id.q3_3)).isChecked();
 
-        Toast.makeText(this, "Your score is: " + score + "/2", Toast.LENGTH_SHORT).show();
+        // Boolean for the correct answer of the third question
+        boolean answer3 = checkBox1 && checkBox2 && checkBox3;
+
+
+        // Sum of correct answers
+        score = (answer1?1:0) + (answer2?1:0) + (answer3?1:0);
+
+        Toast.makeText(this, "Your score is: " + score + "/3", Toast.LENGTH_SHORT).show();
 
 
     }
