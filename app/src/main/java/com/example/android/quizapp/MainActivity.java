f32e4912214fb4617327b9969761cf3dd71531c5
Rani <ranielmaalouf@gmail.com>
2017-04-11 19:52:31 +0200

    Add correction of answers and change checkboxes to radio buttons of the first question.

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.android.quizapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    int score;
+    int questionsAnswered = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void correctQuiz(View view){
+
+        // Boolean for the correct answer of the first question
+        boolean answer1 = ((RadioButton) findViewById(R.id.q1_radioButton3)).isChecked();
+
+
+
+
+        score = (answer1?1:0);
+
+        Toast.makeText(this, "Your score is: " + score + "/1", Toast.LENGTH_SHORT).show();
+
+
     }
 }
